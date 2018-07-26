fe700c4d5fa9e24caaef125d5af72847743f8a51
sun <sunbinglang@sxwdsoft.com>
2018-07-26 17:40:38 +0800

    卡其大傻逼

---
+++ KQ/src/main/java/com/kq/KQ/KqApplication.java
@@ -0,0 +1,12 @@
+package com.kq.KQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KqApplication.class, args);
+	}
+}
